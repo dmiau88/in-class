6ae7bcac01af4df933fd410379d5893c2466f2b3
Daniel Miau <dmiau88@uw.edu>
2021-05-04 22:09:47 -0700

    in class demo

--- day-09/day-09/js/demo.js
+++ day-09/day-09/js/demo.js
@@ -12,7 +12,7 @@ body.appendChild(paragraph);
 
 // use querySelector and css selectors to get the none.css href
 // log it
-let css = document.querySelector('[href]*="css/none.css');
+let css = document.querySelector('[href*="css/none.css"]');
 console.log(css);
 
 
@@ -26,23 +26,52 @@ h1.style.fontSize = '5em';
 //add event listener to the button to track x and y of clicks
 //log them
 //log the target
+
 let button = document.querySelector('button');
 button.addEventListener('click', function() {
-    let message = 'You Clicked' + 
-    event.offsetX + " and " + event.offset.Y;
+    let message = `You clicked ${event.offsetX} and ${event.offsetY}`;
+
+    let clickedElem = event.target;
+
+    console.log(message);
+    console.log(clickedElem);
 });
-console.log(message);
 
 
 
 //key press event listener
 
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'ArrowUp') {
+        console.log("Going up!")
+    }
+});
 
 
+function doSomething(id) {
+    document.querySelector(`#${id}`).addEventListener('click', function(){
+        css.setAttribute('href', `css/${id}.css`);
+    });
+}
 
 //get all style sheet links, log them, then add even listeners to each
+let listItems = document.querySelectorAll('.styles li');
+// console.log(listItems);
 
-
+for(var i in Object.keys(listItems)) {
+    doSomething(listItems[i].id);
+}
 
 
 // track state of clicks
+
+let clickCount = 0;
+document.querySelector('button.clickAgain').addEventListener('click', function() {
+    if(clickCount > 10) {
+        console.log("I think you've had enough");
+    }
+    else {
+        clickCount++;
+        console.log(`You clicked me ${clickCount} times!`);
+    }
+});
